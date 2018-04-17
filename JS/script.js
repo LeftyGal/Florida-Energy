a4d51a6bbda78c7664ffc5d3e270220253360a4a
LeftyGal <egalinou@uw.edu>
2018-04-16 22:10:27 -0700

    add fb buttons

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,13 @@ function myMap() {
 
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyApACqbA7lS9wA2fjlJta5hxQw6wE8TTGM=myMap"></script>
+
+
+ <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
