589956c828578602695ce8673d7fbd4a3f0dc36d
LeftyGal <egalinou@uw.edu>
2018-04-17 21:24:14 -0700

    format html spacing

--- JS/script.js
+++ JS/script.js
@@ -13,17 +13,15 @@ function myMap() {
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyApACqbA7lS9wA2fjlJta5hxQw6wE8TTGM=myMap"></script>
 
 
- <div id="fb-root"></div>
-<script>(function(d, s, id) {
+  (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-
-  <script>window.twttr = (function(d, s, id) {
+}(document, 'script', 'facebook-jssdk'));
+  
+  window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
     t = window.twttr || {};
   if (d.getElementById(id)) return t;
@@ -31,9 +29,12 @@ function myMap() {
   js.id = id;
   js.src = "https://platform.twitter.com/widgets.js";
   fjs.parentNode.insertBefore(js, fjs);
-  t._e = [];
+  
+    t._e = [];
   t.ready = function(f) {
     t._e.push(f);
   };
-  return t;
-}(document, "script", "twitter-wjs"));</script>
+  
+    return t;
+}(document, "script", "twitter-wjs"));
+  
