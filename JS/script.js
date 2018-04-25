2e968922a9cfcf2859ba7569671334d09ab5202f
LeftyGal <egalinou@uw.edu>
2018-04-24 21:12:05 -0700

    add images and edit format html

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,7 @@
 <!-- Google Maps -->
 
-        function myMap() {
-  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: myCenter, zoom: 12};
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var marker = new google.maps.Marker({position:myCenter});
-  marker.setMap(map);
-          
+
+<!-- facebook, twitter buttons -->
   
 
 (function(d, s, id) {
@@ -38,8 +32,4 @@
 
        
   
-          
-          </script>
-        
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyApACqbA7lS9wA2fjlJta5hxQw6wE8TTGM=myMap"></script>
-  
+     
