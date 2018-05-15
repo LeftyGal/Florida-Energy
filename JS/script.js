9d7dd3c901f0b2693986599311b869710ff3da4a
LeftyGal <egalinou@uw.edu>
2018-05-14 20:33:16 -0700

    add side nav

--- JS/script.js
+++ JS/script.js
@@ -67,7 +67,13 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
        
   
      
