b773de232eaad8eb37389902a674f44b2bc1b2ee
LeftyGal <egalinou@uw.edu>
2018-05-15 20:40:22 -0700

    fix map?

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,12 @@
 
 
 function myMap() {
-  var mapCanvas = document.getElementById("googleMap");
-  var mapOptions = {
-    center: new google.maps.LatLng(29.65, -82.32), zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+var mapProp={
+    center:new google.maps.LatLng(29.65, -82.32),
+    zoom:10,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
-
 
 
 (function(d, s, id) {
