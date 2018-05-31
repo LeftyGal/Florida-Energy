10cf6b55923e79a752329d3a64583d0707b7f96d
LeftyGal <egalinou@uw.edu>
2018-05-30 18:27:13 -0700

    add rename buttons

--- JS/vue.js
+++ JS/vue.js
@@ -1,43 +1,16 @@
 
-var graphs = [
-  {name: "renewable",
-  },{name: "totalenergy"
-  },
-]
-
-
-Vue.component('app-datagraphs', {
- props: ['text'],
- template: ''
-})
 
 var app = new Vue({
   el: '#app',
   data: {
-    graphs: datagraphs
+    
   },
   methods: {
-    test: function(){
-      console.log("test was called")
+    showrenewableproduction: function(){
+      console.log("test1 was called")
     },
-    filter: function(){
-      console.log("filter was called")
-  
-      // if checkbox is checked
-      if(document.getElementById("renewable").checked){
-        // set this.graphs to only renewableenergy === true
-        this.graphs = datagraphs.filter(g => t.renewable)
-      } else {
-        // both tables
-        this.graphs = datagraphs   
-             // if checkbox is checked
-         if(document.getElementById("totalenergy").checked){
- // set this.graphs to only totalenergy === true
-           this.graphs = datagraphs.filter(g => t.totalenergy)
-            } else {
-        this.graphs = datagraphs
-            }
-    }
-  }
+    showtotalconsumption: function(){
+    console.log("test2 was called")
+  } 
 }
 })
